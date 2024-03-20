5a0ac4d6d0335cdf249bc8faf5dbb15c89f90b34
alex sanchez <alexjsanchez1020@gmail.com>
2024-03-20 09:27:15 -0400

    removing extra code

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ import Stories from "./pages/Stories";
 import Features from "./pages/Features";
 import Navigation from "./pages/Navigation";
 import Footer from "./pages/Footer";
-import logo from "./assets/shared/desktop/logo.svg";
-
-import arrow from "./assets/shared/desktop/arrow.svg";
-import facebook from "./assets/shared/desktop/facebook.svg";
-import youtube from "./assets/shared/desktop/youtube.svg";
-import twitter from "./assets/shared/desktop/twitter.svg";
-import pinterest from "./assets/shared/desktop/pinterest.svg";
-import instagram from "./assets/shared/desktop/instagram.svg";
-
 import "./App.css";
 import "./index.css";
 
